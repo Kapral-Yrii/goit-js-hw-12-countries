9bbef5adba9eee488163581af5b987ddc2b9f47a
Kapral-Yrii <kapral2701@gmail.com>
2021-08-23 18:36:24 +0300

    complite task

--- src/index.js
+++ src/index.js
@@ -7,26 +7,49 @@ import fetchCountries from './fetchCountries.js'
 
 const refs = {
     input: document.querySelector(".input"),
-    countriesList: document.querySelector(".countries__list")
+    countriesList: document.querySelector(".countries__list"),
+    root: document.querySelector(".root")
 }
 
 function createMarkupListCountries(arr) {
+    refs.countriesList.innerHTML = ""
     const template = arr.map((el) =>`<li>${el.name}</li>`).join("")
     refs.countriesList.insertAdjacentHTML('beforeend', template)
 }
 
 function getFetchData() {
     refs.countriesList.innerHTML = ""
+    refs.root.innerHTML = ""
     const value = refs.input.value
     console.log(value);
     fetchCountries(value)
 }
+function createMarkupCountry(arr) {
+    refs.root.innerHTML = ""
+    const country = arr[0]
+    const languageMarkup = country.languages.map((el) =>`<li>${el.name}</li>`).join("")
+    const countryMarkup =
+    `<h1 class="country__name">${country.name}</h1>
+    <div class="country__thumb">
+        <div class="country__data">
+            <p>Capital: <span>${country.capital}</span></p>
+            <p>Population: <span>${country.population}</span></p>
+            <p>Languages: <ul>${languageMarkup}</ul></p>
+        </div>
+        <div class="country__flag">
+            <img src="${country.flag}" alt="${country.name}">
+        </div>
+    </div>`
+    refs.root.insertAdjacentHTML('beforeend',countryMarkup)
+}
     
 export default function renderCollections(arrCountries) {
-    if(arrCountries.length === 1) {
+    if (arrCountries.length === 1) {
         return createMarkupCountry(arrCountries)
-    } else if(arrCountries.length > 10) {
-        return alert({text: 'Too many matches found. Please enter a more specific query!'})
+    } else if (arrCountries.length > 10) {
+        return alert({ text: 'Too many matches found. Please enter a more specific query!' })
+    } else if (arrCountries.message === 'Not Found') {
+        return error({text: 'Country not found'})
     }
     createMarkupListCountries(arrCountries)
 }
